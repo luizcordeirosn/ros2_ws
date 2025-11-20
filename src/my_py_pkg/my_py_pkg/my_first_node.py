61279796c1188c5e5420eb556c9950013a8e1fd7
Luiz Neto <luizcsneto@outlook.com>
2025-11-20 20:50:07 -0300

    feat: integrate MyNode class into my_first_node

--- src/my_py_pkg/my_py_pkg/my_first_node.py
+++ src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -2,11 +2,24 @@ import rclpy
 from rclpy.node import Node
 
 
+class MyNode(Node):
+    def __init__(self, node_name):
+        super().__init__(node_name)
+        self._counter = 1
+        self.get_logger().info("Hello, ROS2!")
+        self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info(
+            f"Timer callback executed {self._counter} {'time' if self._counter == 1 else 'times'}."
+        )
+        self._counter += 1
+
+
 def main(args=None):
     rclpy.init(args=args)
 
-    node = Node("my_first_node")
-    node.get_logger().info("Hello, ROS2!")
+    node = MyNode("my_first_node")
 
     rclpy.spin(node)
 
