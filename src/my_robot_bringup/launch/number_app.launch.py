f65cb796346442c385f47561f3b5f9c1538e4f5e
Luiz Neto <luizcsneto@outlook.com>
2025-12-26 12:03:56 -0300

    feat: add parameter configuration to launch files

--- src/my_robot_bringup/launch/number_app.launch.py
+++ src/my_robot_bringup/launch/number_app.launch.py
@@ -10,6 +10,10 @@ def generate_launch_description():
         executable="number_publisher",
         name="my_number_publisher",
         remappings=[("/number", "/my_number")],
+        parameters=[
+            {"number": 12},
+            {"timer_period": 1.3},
+        ],
     )
 
     number_counter = Node(
